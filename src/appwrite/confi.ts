4b943f1c3cc07e0810a8158d680b7c7c187ec256
pH-1491 <priyeharsh19@gmail.com>
2025-08-31 00:32:23 +0530

    added store to the project

--- src/appwrite/confi.ts
+++ src/appwrite/confi.ts
@@ -1,145 +1,120 @@
-import config from '../conf/config.ts'
-import { Client, Databases,Storage,Query, ID } from "appwrite";
+import config from '../conf/config'
+import { Client, Databases, Storage, Query, ID } from "appwrite";
 
-export class Service{
+export class Service {
     client = new Client();
-    databases;
-    bucket;
+    databases: Databases;
+    bucket: Storage;
 
-    constructor(){
+    constructor() {
         this.client
             .setEndpoint(config.appwriteUrl)
             .setProject(config.appwriteProjectId);
+
         this.databases = new Databases(this.client);
         this.bucket = new Storage(this.client);
     }
 
-    async createPost({title,slug,content,featuredImage,status,userId}:{
-        title:string,
-        slug:string,
-        content:string,
-        featuredImage:string,
-        status:string,
-        userId:string,
-    })  {
-        try{
-            return await this.databases.createDocument(
-                config.appwriteDatabaseId,
-                config.appwriteCollectionId,
-                slug,  //slug is taken as the document id
-                {
-                    title,
-                    content,
-                    featuredImage,
-                    status,
-                    userId,
-                }
-
-            )
-
-        }catch(err){
-            console.log(err);
-        }
+    async createPost({
+                         title,
+                         slug,
+                         content,
+                         featuredImage,
+                         status,
+                         userId,
+                     }: {
+        title: string;
+        slug: string;
+        content: string;
+        featuredImage: string;
+        status: string;
+        userId: string;
+    }) {
+        return await this.databases.createDocument(
+            config.appwriteDatabaseId,
+            config.appwriteCollectionId,
+            slug, // slug used as document ID
+            {
+                title,
+                content,
+                featuredImage,
+                status,
+                userId,
+            }
+        );
     }
-
 
     async updatePost(
         slug: string,
-        { title, content, featuredImage, status, userId }:
-        { title: string; content: string; featuredImage: string; status: string; userId: string }
+        {
+            title,
+            content,
+            featuredImage,
+            status,
+            userId,
+        }: {
+            title: string;
+            content: string;
+            featuredImage: string;
+            status: string;
+            userId: string;
+        }
     ) {
-        try {
-            return await this.databases.updateDocument(
-                config.appwriteDatabaseId,
-                config.appwriteCollectionId,
-                slug,
-                {
-                    title,
-                    content,
-                    featuredImage,
-                    status
-                }
-            );
-        } catch (err) {
-            console.log(err);
-        }
+        return await this.databases.updateDocument(
+            config.appwriteDatabaseId,
+            config.appwriteCollectionId,
+            slug,
+            {
+                title,
+                content,
+                featuredImage,
+                status,
+                userId, // included for consistency
+            }
+        );
     }
 
-    async deletePost(slug: string){
-        try{
-            await this.databases.deleteDocument(
-                config.appwriteDatabaseId,
-                config.appwriteCollectionId,
-                slug
-            );
-            return true;
-        }catch(err){
-            console.log(err);
-            return false;
-        }
+    async deletePost(slug: string) {
+        await this.databases.deleteDocument(
+            config.appwriteDatabaseId,
+            config.appwriteCollectionId,
+            slug
+        );
+        return true;
     }
 
-    async getPost(slug: string){
-        try{
-            return await this.databases.getDocument(
-                config.appwriteDatabaseId,
-                config.appwriteCollectionId,
-                slug
-            );
-        }catch(err){
-            console.log(err);
-            return false;
-        }
+    async getPost(slug: string) {
+        return await this.databases.getDocument(
+            config.appwriteDatabaseId,
+            config.appwriteCollectionId,
+            slug
+        );
     }
 
-
-    async getPosts(queries = [Query.equal("status","active")]){
-        try{
-            return this.databases.listDocuments(
-                config.appwriteDatabaseId,
-                config.appwriteCollectionId,
-                queries
-            )
-
-        }catch(err){
-            console.log(err);
-            return false;
-        }
-
+    async getPosts(queries = [Query.equal("status", ["active"])]) {
+        return await this.databases.listDocuments(
+            config.appwriteDatabaseId,
+            config.appwriteCollectionId,
+            queries
+        );
     }
 
-    //file upload service
-    async uploadFile(file: File){
-        try{
-            return await this.bucket.createFile(
-                config.appwriteBucketId,
-                ID.unique(),
-                file
-            )
-        }catch(err){
-            console.log(err);
-            return false;
-        }
-    }
-
-    async deleteFile(fileId:string){
-        try{
-            await this.bucket.deleteFile(
-                config.appwriteBucketId,
-                fileId
-            )
-            return true;
-        }catch(err){
-            console.log(err);
-            return false;
-        }
-    }
-
-    getFilePreview(fileId:string){
-        return this.bucket.getFilePreview(
+    // ---- File upload service ----
+    async uploadFile(file: File) {
+        return await this.bucket.createFile(
             config.appwriteBucketId,
-            fileId
-        )
+            ID.unique(),
+            file
+        );
+    }
+
+    async deleteFile(fileId: string) {
+        await this.bucket.deleteFile(config.appwriteBucketId, fileId);
+        return true;
+    }
+
+    getFilePreview(fileId: string) {
+        return this.bucket.getFilePreview(config.appwriteBucketId, fileId);
     }
 }
 
